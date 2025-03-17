c149868856d9c36247ce1b3438b14603530579d7
Matthew Ashton Knochel <ffeufhd5j@relay.firefox.com>
2025-03-16 20:53:34 -0600

    fixed output document

--- launch_server/bin/launch.sh
+++ launch_server/bin/launch.sh
@@ -4,6 +4,7 @@ killall kindle_server
 iptables -A INPUT -p tcp --dport 3000 -j ACCEPT
 (${EXTENSION}/bin/kindle_server) </dev/null &>/dev/null &
 
-FILENAME="/mnt/us/documents/Network_Info_$(date +'%Y%m%d_%H%M%S').txt"
+IP=$(ifconfig wlan0 | grep -o "inet addr:[0-9.]*" | cut -d: -f2)
+FILENAME="/mnt/us/documents/Listening_At_${IP}.txt"
 
 ifconfig > "$FILENAME"
