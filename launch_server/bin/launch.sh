1912dc6cb76c32531e40033bd55e6ff707dc42e3
Matthew Ashton Knochel <ffeufhd5j@relay.firefox.com>
2025-03-16 20:45:47 -0600

    added networkinfo file creation to launch.sh

--- launch_server/bin/launch.sh
+++ launch_server/bin/launch.sh
@@ -3,3 +3,7 @@ EXTENSION="/mnt/us/extensions/launch_server"
 killall kindle_server
 iptables -A INPUT -p tcp --dport 3000 -j ACCEPT
 (${EXTENSION}/bin/kindle_server) </dev/null &>/dev/null &
+
+FILENAME="/mnt/us/documents/Network_Info_$(date +'%Y%m%d_%H%M%S').txt"
+
+ifconfig > "$FILENAME"
